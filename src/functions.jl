d1dc2977778ee0b4b0554204d05e238122fb1b8d
Ikuma Oka <ikuma1018soccer06@gmail.com>
2021-05-25 19:14:44 +0900

    add 3layersNN

--- src/functions.jl
+++ src/functions.jl
@@ -5,3 +5,12 @@ end
 function sigmoid(x)
     return 1 ./ (1 .+ exp.(-x))
 end
+
+function relu(x)
+    return max(0, x)
+end
+
+# 恒等関数
+function identity(x)
+    return x
+end
--- test/functionsTest.jl
+++ test/functionsTest.jl
@@ -11,5 +11,19 @@ function testSigmoid()
     @test sigmoid([-1.0, 1.0, 2.0]) ≈ [0.2689414213699951, 0.7310585786300049, 0.8807970779778823] atol=0.00001
 end
 
+function testRelu()
+    @test relu(3)    == 3
+    @test relu(-1.5) == 0
+    @test relu(0)    == 0
+end
+
+function testIdentity()
+    @test identity(5)    == 5
+    @test identity(-4.0) == -4.0
+    @test identity([1 2 3]) == [1 2 3]
+end
+
 testStep()
 testSigmoid()
+testRelu()
+testIdentity()
