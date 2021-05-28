e61f86e4ed402f8cfe181ca4700903c69697cb24
Ikuma Oka <ikuma1018soccer06@gmail.com>
2021-05-28 16:45:28 +0900

    create notebooks dir

--- src/inferenceMNIST.jl
+++ src/inferenceMNIST.jl
@@ -34,7 +34,9 @@ accuracyCnt = 0
 for i in 1:length(x[28, 28, :])
     Xi =  reshape(x[:, :, i]', length(x[:, :, i]), 1)
     y = predict(network, Xi')
+    println(y)
     p = findmax(y)[2][2]
+    println(p)
     if p == t[i]
         global accuracyCnt += 1
     end
