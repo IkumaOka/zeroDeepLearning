636983cff3f3eda6548dcf361653589afa2cf5a9
Ikuma Oka <ikuma1018soccer06@gmail.com>
2021-05-31 13:22:08 +0900

    add crossEntropyErrorFunc

--- test/errorFunctionsTest.jl
+++ test/errorFunctionsTest.jl
@@ -1,13 +1,24 @@
 using Test
 include("../utils/errorFunctions.jl")
 
-function testmeanSquared()
+function testMeanSquared()
     t = [0 0 1 0 0 0 0 0 0 0]
     y = [0.1 0.05 0.6 0.0 0.05 0.1 0.0 0.1 0.0 0.0]
     @test meanSquared(y, t) ≈ 0.0975 atol=0.00001
 
     y = [0.1 0.05 0.1 0.0 0.05 0.1 0.0 0.6 0.0 0.0]
-    @test meanSquared(y, t) ≈0.5975 atol=0.00001
+    @test meanSquared(y, t) ≈ 0.5975 atol=0.00001
 end
 
-testmeanSquared()
+function testCrossEntropy()
+    t = [0 0 1 0 0 0 0 0 0 0]
+    y = [0.1 0.05 0.6 0.0 0.05 0.1 0.0 0.1 0.0 0.0]
+    @test crossEntropy(y, t) ≈ 0.51082545709933802 atol=0.00001
+
+    y = [0.1 0.05 0.1 0.0 0.05 0.1 0.0 0.6 0.0 0.0]
+    @test crossEntropy(y, t) ≈ 2.3025840929945458 atol=0.00001
+end
+
+
+testMeanSquared()
+testCrossEntropy()
--- utils/errorFunctions.jl
+++ utils/errorFunctions.jl
@@ -1,3 +1,8 @@
 function meanSquared(y, t)
     return 0.5 .* sum((y .- t) .^ 2)
 end
+
+function crossEntropy(y, t)
+    delta = 1e-7
+    return -sum(t .* log.(y .+ delta))
+end
